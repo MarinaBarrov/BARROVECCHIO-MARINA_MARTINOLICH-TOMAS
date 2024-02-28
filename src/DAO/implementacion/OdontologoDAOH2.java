a008b4a72127a8ca6b107d5e1fc6f997fa3e1c44
MarinaBarrov <maari-b@live.com.ar>
2024-02-27 22:56:47 -0300

    getters y setters e implementacion H2

--- src/DAO/implementacion/OdontologoDAOH2.java
+++ src/DAO/implementacion/OdontologoDAOH2.java
@@ -1,4 +1,85 @@
 package DAO.implementacion;
 
-public class OdontologoDAOH2 {
+import DAO.BD;
+import DAO.IDao;
+import Entidades.Odontologo;
+import org.apache.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OdontologoDAOH2 implements IDao<Odontologo> {
+
+    private static final Logger LOGGER = Logger.getLogger(OdontologoDAOH2.class);
+
+    @Override
+    public Odontologo guardar(Odontologo odontologo) {
+        LOGGER.info("Estamos persistiendo un odontologo");
+
+        Connection connection = null;
+
+        try {
+
+            connection = BD.getConnection();
+
+            PreparedStatement pStmt = connection.prepareStatement("INSERT INTO ODONTOLOGOS (MATRICULA, NOMBRE, APELLIDO)  " +
+                    "VALUES(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+            pStmt.setString(1, odontologo.getNumMatricula());
+            pStmt.setString(2, odontologo.getNombre());
+            pStmt.setString(3, odontologo.getApellido());
+            pStmt.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return odontologo;
+    }
+
+
+    @Override
+    public List<Odontologo> listarTodos() {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        List<Odontologo> odontologos = new ArrayList<>();
+
+        try {
+            connection = BD.getConnection();
+
+            preparedStatement = connection.prepareStatement("SELECT * FROM ODONTOLOGOS");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String matricula = resultSet.getString("matricula");
+                String nombre = resultSet.getString("nombre");
+                String apellido = resultSet.getString("apellido");
+
+                Odontologo odontologo = new Odontologo();
+                odontologo.setId(id);
+                odontologo.setNumMatricula(matricula);
+                odontologo.setNombre(nombre);
+                odontologo.setApellido(apellido);
+
+                odontologos.add(odontologo);
+            }
+
+            preparedStatement.close();
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
+        }
+        return odontologos;
+    }
 }
+
--- src/Entidades/Odontologo.java
+++ src/Entidades/Odontologo.java
@@ -5,4 +5,54 @@ public class Odontologo {
     private String numMatricula;
     private String nombre;
     private String apellido;
+
+    public Odontologo() {
+    }
+
+    public Odontologo(String numMatricula, String nombre, String apellido) {
+        this.numMatricula = numMatricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Odontologo(Integer id, String numMatricula, String nombre, String apellido) {
+        this.id = id;
+        this.numMatricula = numMatricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumMatricula() {
+        return numMatricula;
+    }
+
+    public void setNumMatricula(String numMatricula) {
+        this.numMatricula = numMatricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
 }
